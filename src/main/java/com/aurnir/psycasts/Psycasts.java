0054bdc1490b1128f3ab91fb94edd5314ba45f50
unknown <aurnirdev@gmail.com>
2021-05-26 13:52:52 -0500

    Initial Commit. Set up project file

---
+++ src/main/java/com/aurnir/psycasts/Psycasts.java
@@ -0,0 +1,11 @@
+package com.aurnir.psycasts;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Psycasts implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+		
+	}
+}
